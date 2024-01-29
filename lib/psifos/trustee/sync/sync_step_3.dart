c13f6bd3eff8a0726fc27c1b1fdcc8c157da4f20
lomolisso <raul.delafuente@ing.uchile.cl>
2024-01-29 14:32:22 -0300

    replaced dynamic for bigint on step 3

--- lib/psifos/trustee/sync/sync_step_3.dart
+++ lib/psifos/trustee/sync/sync_step_3.dart
@@ -5,13 +5,11 @@ import 'package:psifos_mobile_crypto/crypto/ecc/ec_tdkg/ec_tdkg.dart';
 
 class TrusteeSyncStep3 {
   /* parses step 3 input into usable classes */
-  static Map<String, dynamic> parseInput(Map<String, dynamic> input) {
+  static List<BigInt> parseInput(Map<String, dynamic> input) {
     List<BigInt> recv_shares = input['recv_shares']
         .map((e) => BigInt.parse(e['encrypted_share']))
         .toList();
-    return {
-      'recv_shares': recv_shares,
-    };
+    return recv_shares;
   }
 
   static Map<String, String> handle(List<BigInt> recv_shares, int threshold,
